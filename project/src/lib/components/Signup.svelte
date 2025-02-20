978ae06f85aad828fc9c45ecae756f5f23394cf4
multi05369 <tentingsanook@gmail.com>
2025-02-20 09:55:50 +0700

    Edit Signup Page

--- project/src/lib/components/Signup.svelte
+++ project/src/lib/components/Signup.svelte
@@ -32,10 +32,12 @@
 	}
 </script>
 
-<div class="flex min-h-screen items-center justify-center bg-base-200 px-4 transition-opacity duration-500 ease-in-out opacity-100">
-	<div class="w-full max-w-md rounded-xl bg-base-100 p-6 shadow-xl">
-		<h1 class="text-center text-3xl font-bold text-primary">Sign Up</h1>
-		
+<div
+	class="bg-base-200 flex min-h-screen items-center justify-center px-4 opacity-100 transition-opacity duration-500 ease-in-out"
+>
+	<div class="bg-base-100 w-full max-w-md rounded-xl p-6 shadow-xl">
+		<h1 class="text-primary text-center text-3xl font-bold">Sign Up</h1>
+
 		{#if error}
 			<div class="alert alert-error mt-4">{error}</div>
 		{/if}
@@ -45,34 +47,95 @@
 		{/if}
 
 		<form class="mt-6 space-y-6">
-			<label class="form-control w-full">
-				<span class="label-text">Name</span>
-				<input type="text" class="input input-bordered w-full" bind:value={name} required />
+			<label class="input input-bordered form-control w-full">
+				<svg
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 16 16"
+    fill="currentColor"
+    class="h-4 w-4 opacity-70">
+    <path
+      d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6ZM12.735 14c.618 0 1.093-.561.872-1.139a6.002 6.002 0 0 0-11.215 0c-.22.578.254 1.139.872 1.139h9.47Z" />
+  </svg>
+				<input type="text" class="flex w-full" bind:value={name} required placeholder="Name*"/>
 			</label>
 
-			<label class="form-control w-full">
-				<span class="label-text">Email</span>
-				<input type="email" class="input input-bordered w-full" bind:value={email} required />
+
+			<label class="input input-bordered form-control flex w-full">
+				<svg
+					xmlns="http://www.w3.org/2000/svg"
+					viewBox="0 0 16 16"
+					fill="currentColor"
+					class="h-4 w-4 opacity-70"
+				>
+					<path
+						d="M2.5 3A1.5 1.5 0 0 0 1 4.5v.793c.026.009.051.02.076.032L7.674 8.51c.206.1.446.1.652 0l6.598-3.185A.755.755 0 0 1 15 5.293V4.5A1.5 1.5 0 0 0 13.5 3h-11Z"
+					/>
+					<path
+						d="M15 6.954 8.978 9.86a2.25 2.25 0 0 1-1.956 0L1 6.954V11.5A1.5 1.5 0 0 0 2.5 13h11a1.5 1.5 0 0 0 1.5-1.5V6.954Z"
+					/>
+				</svg>
+				<input type="email" class="flex w-full" bind:value={email} required placeholder="Email*" />
 			</label>
 
-			<label class="form-control w-full">
-				<span class="label-text">Password</span>
-				<input type="password" class="input input-bordered w-full" bind:value={password} required />
+			<div class="mt-4"></div>
+
+			<label class="input input-bordered form-control flex w-full">
+				<svg
+					xmlns="http://www.w3.org/2000/svg"
+					viewBox="0 0 16 16"
+					fill="currentColor"
+					class="h-4 w-4 opacity-70"
+				>
+					<path
+						fill-rule="evenodd"
+						d="M14 6a4 4 0 0 1-4.899 3.899l-1.955 1.955a.5.5 0 0 1-.353.146H5v1.5a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1-.5-.5v-2.293a.5.5 0 0 1 .146-.353l3.955-3.955A4 4 0 1 1 14 6Zm-4-2a.75.75 0 0 0 0 1.5.5.5 0 0 1 .5.5.75.75 0 0 0 1.5 0 2 2 0 0 0-2-2Z"
+						clip-rule="evenodd"
+					/>
+				</svg>
+				<input
+					type="password"
+					class="flex"
+					bind:value={password}
+					required
+					placeholder="Password*"
+				/>
 			</label>
 
-			<label class="form-control w-full">
-				<span class="label-text">Confirm Password</span>
-				<input type="password" class="input input-bordered w-full" bind:value={confirmPassword} required />
+			<div class="mt-4"></div>
+
+			<label class="input input-bordered form-control w-full">
+				<svg
+					xmlns="http://www.w3.org/2000/svg"
+					viewBox="0 0 16 16"
+					fill="currentColor"
+					class="h-4 w-4 opacity-70"
+				>
+					<path
+						fill-rule="evenodd"
+						d="M14 6a4 4 0 0 1-4.899 3.899l-1.955 1.955a.5.5 0 0 1-.353.146H5v1.5a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1-.5-.5v-2.293a.5.5 0 0 1 .146-.353l3.955-3.955A4 4 0 1 1 14 6Zm-4-2a.75.75 0 0 0 0 1.5.5.5 0 0 1 .5.5.75.75 0 0 0 1.5 0 2 2 0 0 0-2-2Z"
+						clip-rule="evenodd"
+					/>
+				</svg>
+				<input
+					type="password"
+					class="flex w-full"
+					bind:value={confirmPassword}
+					required
+					placeholder="Confirm Password*"
+				/>
 			</label>
 
-			<label class="form-control w-full">
-				<span class="label-text">Phone</span>
-				<input type="text" class="input input-bordered w-full" bind:value={phone} />
+
+			<label class="input input-bordered form-control w-full">
+				<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="currentColor" d="M2 7h2v7H2zm18 3h2v7h-2zm-4-7.99L8 2c-1.1 0-2 .9-2 2v16c0 1.1.9 2 2 2h8c1.1 0 2-.9 2-2V4c0-1.1-.9-1.99-2-1.99M16 17H8V7h8z"/></svg>
+				  
+				  
+				<input type="text" class="flex w-full" bind:value={phone} required placeholder="Phone number*" />
 			</label>
 
+
 			<label class="form-control w-full">
-				<span class="label-text">Address</span>
-				<textarea class="textarea textarea-bordered w-full" bind:value={address}></textarea>
+				<textarea class="textarea textarea-bordered w-full" bind:value={address} placeholder="Address*"></textarea>
 			</label>
 
 			<div class="mt-4"></div>
